24136e005534af666d0d41e4988719b5817bad58
Sahda Samier <sahdasamier@gmail.com>
2022-10-04 18:27:47 +0200

    edited

--- src/components/Navbar/index.tsx
+++ src/components/Navbar/index.tsx
@@ -1,4 +1,4 @@
-import { useNavigate } from "react-router-dom";
+import {useNavigate} from "react-router-dom";
 import AppBar from "@mui/material/AppBar";
 import Box from "@mui/material/Box";
 import Toolbar from "@mui/material/Toolbar";
@@ -6,44 +6,69 @@ import Typography from "@mui/material/Typography";
 import Button from "@mui/material/Button";
 import ProfileDropdown from "../ProfileDropdown";
 
-const Navbar = ({ isAuth }: any) => {
-  const navigate = useNavigate();
-  return (
-    <Box sx={{ flexGrow: 1, width: "100%", overflow: "hidden" }}>
-      <AppBar
-        position="static"
-        sx={{ backgroundColor: "grey.300", color: "black" }}
-      >
-        <Toolbar>
-          <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
-            Trust Claims
-          </Typography>
-          <Box sx={{ display: "flex", justifyContent: "center", columnGap: 3 }}>
-            {isAuth ? (
-              <>
-                {/* <Button color="inherit" onClick={() => navigate("/")}>
-                  Create Claim
-                </Button> */}
-                <Button color="inherit" onClick={() => navigate("/search")}>
-                  Search
-                </Button>
-                <ProfileDropdown/>
-              </>
-            ) : (
-              <>
-                <Button color="inherit" onClick={() => navigate("/login")}>
-                  Login
-                </Button>
-                <Button color="inherit" onClick={() => navigate("/register")}>
-                  Register
-                </Button>
-              </>
-            )}
-          </Box>
-        </Toolbar>
-      </AppBar>
-    </Box>
-  );
+const Navbar = ({isAuth} : any) => {
+    const navigate = useNavigate();
+    return (
+        <Box sx={
+            {
+                flexGrow: 1,
+                width: "100%",
+                overflow: "hidden"
+            }
+        }>
+            <AppBar position="static"
+                sx={
+                    {
+                        backgroundColor: "grey.300",
+                        color: "black"
+                    }
+            }>
+                <Toolbar>
+                    <Typography variant="h6" component="div"
+                        sx={
+                            {flexGrow: 1}
+                    }>
+                        Trust Claims
+                    </Typography>
+                    <Box sx={
+                        {
+                            display: "flex",
+                            justifyContent: "center",
+                            columnGap: 3
+                        }
+                    }>
+                        {
+                        isAuth ? (
+                            <>
+                                <Button color="inherit"
+                                    onClick={
+                                        () => navigate("/search")
+                                }>
+                                    Search
+                                </Button>
+                                <ProfileDropdown/>
+                            </>
+                        ) : (
+                            <>
+                                <Button color="inherit"
+                                    onClick={
+                                        () => navigate("/login")
+                                }>
+                                    Login
+                                </Button>
+                                <Button color="inherit"
+                                    onClick={
+                                        () => navigate("/register")
+                                }>
+                                    Register
+                                </Button>
+                            </>
+                        )
+                    } </Box>
+                </Toolbar>
+            </AppBar>
+        </Box>
+    );
 };
 
 export default Navbar;
