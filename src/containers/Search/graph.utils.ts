40dcc47c3e96c254305c48e98be0aa8e75740d6c
Golda <gvelez17@gmail.com>
2023-04-03 22:46:50 +0000

    Merge master to  remote master

--- src/containers/Search/graph.utils.ts
+++ src/containers/Search/graph.utils.ts
@@ -27,37 +27,27 @@ const parseClaims = (claims: any) => {
   claims.forEach((claim: any) => {
     // adding subject node
     if (claim.subject) {
-<<<<<<< HEAD
-      const uri = claim.subject; //new URL(claim.subject);
-      const label = uri; //getLabel(uri);
-=======
       let uri: any;
       if (isValidUrl(claim.subject)) uri = new URL(claim.subject);
       else uri = claim.subject;
 
       const label = getLabel(uri);
->>>>>>> 6f17cc385ef4005fca6ffd7468b9df0152e9f102
 
       elements.push({
         data: {
           id: claim.subject,
           label: label,
         },
-      });
+      }
     }
 
     // adding object node
     if (claim.object) {
-<<<<<<< HEAD
-      const uri = claim.object; // new URL(claim.object);
-      const label = uri; // getLabel(uri);
-=======
       let uri: any;
       if (isValidUrl(claim.object)) uri = new URL(claim.object);
       else uri = claim.object;
 
       const label = getLabel(uri);
->>>>>>> 6f17cc385ef4005fca6ffd7468b9df0152e9f102
 
       elements.push({
         data: {
