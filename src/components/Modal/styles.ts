8c8ae54a53d46c11776ec8d47ce59149a232a46e
Nishant <youngnishant@outlook.com>
2023-04-05 15:41:31 +0530

    fixed modal not opening issue

--- src/components/Modal/styles.ts
+++ src/components/Modal/styles.ts
@@ -1,8 +1,6 @@
 const styles = {
   container: {
-    
-    color:'	#000000',
- 
+    color: "#000000",
     position: "absolute",
     top: "50%",
     left: "50%",
@@ -11,38 +9,31 @@ const styles = {
     width: "100%",
     height: "60%",
     maxHeight: "450px",
-    
     bgcolor: "background.paper",
     border: "2px solid #004",
     borderRadius: "4px",
     boxShadow: 20,
     p: 4,
-    
     overflow: "hidden",
     overflowY: "auto",
   },
   detailField: {
-
-     display: "flex-wrap",
+    display: "flex",
     justifyContent: "space-between",
     alignItems: "center",
+    columnGap: "40px",
     borderBottom: "1px solid #000",
     padding: "5px 10px",
     backgroud: "#eeeeee",
-    
   },
   fieldContent: {
     overflow: "hidden",
     overflowWrap: "break-word",
-    
   },
   createClaimBtn: {
-  
     display: "block",
     margin: "20px 0 5px auto",
   },
 };
 
 export default styles;
-
-
