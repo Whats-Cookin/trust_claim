cc56c4b056547d867b82aaa7b689b71f348d2448
romeoscript <romeobourne211@gmail.com>
2023-04-06 13:08:51 +0100

    feat:comment enabled commenting

--- src/components/Modal/index.tsx
+++ src/components/Modal/index.tsx
@@ -1,16 +1,13 @@
 import Box from "@mui/material/Box";
 import Typography from "@mui/material/Typography";
 import MUIModal from "@mui/material/Modal";
-import { useState } from "react";
+// import { useState } from ß"react";
 
 import { camelCaseToSimpleString } from "../../utils/string.utils";
 
 import styles from "./styles";
-import Comment from "../comment/comment";
-const comments ={
-  id:1,
-  items:[]
-};
+import Comment from "../comment/Comment";
+
 const Modal = ({ open, setOpen, selectedClaim }: any) => {
   const handleClose = () => setOpen(false);
   if (!selectedClaim) return null;
@@ -24,10 +21,12 @@ const Modal = ({ open, setOpen, selectedClaim }: any) => {
     "lastUpdatedAt",
     "effectiveDate",
   ];
-const [commentData, setCommentData] = useState(comments)
+//const [commentData, setCommentData] = useState(comments)
   return (
+   
     <MUIModal open={open} onClose={handleClose}>
-      <Box sx={{ ...styles.container, maxHeight: "80vh" }}>
+       <div className="h-[80vh] absolute left-[50%] top-[50%] w-full" style={{transform: 'translate(-50%, -50%)',maxWidth:'800px'}}>
+      <Box sx={{ ...styles.container }}>
         <Typography variant="h4" component="h2" sx={{ marginBottom: 4 }}>
           Claim
         </Typography>
@@ -44,10 +43,13 @@ const [commentData, setCommentData] = useState(comments)
               </Box>
             )
           )}
-        
+      
       </Box>
-   
+      <Comment />
+      </div>
+
     </MUIModal>
+   
   );
 };
 
--- src/components/comment/Comment.tsx
+++ src/components/comment/Comment.tsx
@@ -1,25 +1,94 @@
-import {useState} from "react";
+import { useState } from "react";
 import SendIcon from "@mui/icons-material/Send";
 
-const Comment = () => {
-    const [input, setInput] = useState('')
+interface comment {
+  body: string;
+  comment:Array<comment>
+}
+const dummyComments: Array<comment> = [
+  {
+    body: "this is comment1",
+    comment:[]
+  },
+  {
+    body: "this is comment2",
+    comment:[]
+  },
+  {
+    body: "this is comment3",
+    comment:[]
+  },
+];
+
+export default function ClaimComment() {
+  const [comments, setComments] = useState(dummyComments);
+  const [commentBody, setCommentBody] = useState("");
+
+  const onComment = () => {
+    const newComment: comment = {
+      body: commentBody,
+      comment:[]
+    };
+    setComments((prev) => [newComment, ...prev]);
+  };
   return (
-    <div>
-      <div>
+    <>
+      <div className="fixed bottom-0 flex w-full   ">
+        <div className="w-3/5">
+          <input
+            type="text"
+            className="border-[1px] border-zinc-400 p-4 w-full  outline-none"
+            autoFocus
+            value={commentBody}
+            onChange={(event) => setCommentBody(event.target.value)}
+            placeholder="type comment"
+          />
+        </div>
+        <button
+          onClick={() => onComment()}
+          className="border-[1px] rounded-full border-zinc-400 p-[0.5rem] bg-[#80b8bd]"
+        >
+          <SendIcon />
+        </button>
+      </div>
+      <div className="fixed right-[-38%] flex flex-col gap-4 p-[0.5rem] rounded-md w-[300px] bg-white">
+        {comments.map((comment) => (
+          <CommentItem comment={comment} />
+        ))}
+      </div>
+    </>
+  );
+}
+
+interface CommentItemProps {
+  comment: comment;
+}
+const CommentItem = ({ comment }: CommentItemProps) => {
+  const [isReplying, setIsReplying] = useState(false);
+//   const [first, setfirst] = useState(second)
+  return (
+    <div className="flex flex-col border-[1px] border-zinc-400 ">
+      <span>{comment.body}</span>
+      {isReplying? (
+         <button className="border-[1px] rounded-full border-zinc-400 w-2/5"
+         onClick={()=> setIsReplying(false)}
+         >
+           cancel
+         </button>
+      ):(
+        <button className="border-[1px] rounded-full border-zinc-400 w-2/5"
+        onClick={()=> setIsReplying(true )}
+        >
+          Reply
+        </button>
+      )}
+      {isReplying && (
         <input
           type="text"
-          className="border-[1px] border-zin-400 p-4 w-3/4"
-          autoFocus
-          value={input}
-          onChange={(e) => setInput(e.target.value)}
-          placeholder="type comment"
+          placeholder="what are your thoughts?"
+          className="border-[1px] border-zinc-400 p-4 w-full  outline-none"
         />
-      </div>
-      <button className="border-[1px] rounded-full border-zinc-400">
-        <SendIcon />
-      </button>
+      )}
     </div>
   );
 };
-
-export default Comment;
