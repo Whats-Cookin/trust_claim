2da9fa2604f5ecf349e3f6b52750b1ab3d4f3a98
Sahda Samier <sahdasamier@gmail.com>
2023-04-07 22:39:31 +0200

    format2

--- src/containers/claimNode/index.tsx
+++ src/containers/claimNode/index.tsx
@@ -1,25 +1,22 @@
-import React, { useState } from "react";
-import styles from "./../claimNode/styles";
-import { Box, Button, TextField, Typography, Slider, styled } from "@mui/material";
-import { PublishClaim } from "../../composedb/compose";
-import { useTheme } from "@mui/material/styles";
-import MUIModal from "@mui/material/Modal";
+import React, { useState } from 'react'
+import styles from './../claimNode/styles'
+import { Box, Button, TextField, Typography, Slider, styled } from '@mui/material'
+import { PublishClaim } from '../../composedb/compose'
+import { useTheme } from '@mui/material/styles'
+import MUIModal from '@mui/material/Modal'
 
+const NewClaimPopup: React.FC<Props> = ({ open, setOpen }) => {
+  const handleClose = () => setOpen(false)
+  const [subject, setSubject] = useState('')
+  const [claim, setClaim] = useState('')
+  const [object, setObject] = useState('')
+  const [statement, setStatement] = useState('')
+  const [sourceURI, setSourceURI] = useState('')
 
-const NewClaimPopup: React.FC<Props> = ({ open, setOpen}) => {
-  const handleClose = () => setOpen(false);
-  const [subject, setSubject] = useState("");
-  const [claim, setClaim] = useState("");
-  const [object, setObject] = useState("");
-  const [statement, setStatement] = useState("");
-  const [sourceURI, setSourceURI] = useState("");
+  const theme = useTheme()
 
-  const theme = useTheme();
-
-  const handleSubmission = async (
-    e: React.MouseEvent<HTMLButtonElement, MouseEvent>
-  ) => {
-    e.preventDefault();
+  const handleSubmission = async (e: React.MouseEvent<HTMLButtonElement, MouseEvent>) => {
+    e.preventDefault()
     if (subject && claim) {
       try {
         const payload = {
@@ -27,91 +24,90 @@ const NewClaimPopup: React.FC<Props> = ({ open, setOpen}) => {
           claim,
           object,
           statement,
-          sourceURI,
-        };
-        const res = await PublishClaim(payload);
+          sourceURI
+        }
+        const res = await PublishClaim(payload)
         if (res.status === 201) {
-          handleClose();
+          handleClose()
         }
       } catch (err: any) {
-        console.error("err", err.response.data.message);
+        console.error('err', err.response.data.message)
       }
     }
-  };
+  }
 
   const inputFieldLabelArr = [
     {
-      label: "Subject",
+      label: 'Subject',
       value: subject,
       setter: setSubject,
-      type: "text",
+      type: 'text'
     },
     {
-      label: "Claim",
+      label: 'Claim',
       value: claim,
       setter: setClaim,
-      type: "text",
+      type: 'text'
     },
     {
-      label: "Object",
+      label: 'Object',
       value: object,
       setter: setObject,
-      type: "text",
+      type: 'text'
     },
     {
-      label: "Statement",
+      label: 'Statement',
       value: statement,
       setter: setStatement,
-      type: "text",
+      type: 'text'
     },
     {
-      label: "Source URI",
+      label: 'Source URI',
       value: sourceURI,
       setter: setSourceURI,
-      type: "text",
-    },
-  ];
+      type: 'text'
+    }
+  ]
 
   if (!open) {
-    return null;
+    return null
   }
 
   return (
     <MUIModal open={open} onClose={handleClose}>
-     <Box sx={{ ...styles.formContainer, maxHeight: "80vh" }}>
-
-      <Typography variant="h4" sx={{ marginBottom: "16px" }}>
-        New Claim
-      </Typography>
-      {inputFieldLabelArr.map(({ label, value, setter, type }, i) => (
-        <TextField
-          key={i}
-          value={value}
-          fullWidth
-          margin="normal"
-          label={label}
-          type={type}
-          onChange={(event) => setter(event.target.value)}
-        />
-      ))}
-      <Button
-        onClick={handleSubmission}
-        variant="contained"
-        size="large"
-        sx={{
-          marginTop: "16px",
-          backgroundColor: theme.palette.success.main,
-          color: "#fff",
-          "&:hover": {
-            backgroundColor: theme.palette.success.dark,
-          },
-        }}
-      >
-        Submit
-      </Button>
+      <Box sx={{ ...styles.formContainer, maxHeight: '80vh' }}>
+        <Typography variant='h4' sx={{ marginBottom: '16px' }}>
+          New Claim
+        </Typography>
+        {inputFieldLabelArr.map(({ label, value, setter, type }, i) => (
+          <TextField
+            key={i}
+            value={value}
+            fullWidth
+            margin='normal'
+            label={label}
+            type={type}
+            onChange={event => setter(event.target.value)}
+          />
+        ))}
+        <Button
+          onClick={handleSubmission}
+          variant='contained'
+          size='large'
+          sx={{
+            marginTop: '16px',
+            backgroundColor: theme.palette.success.main,
+            color: '#fff',
+            '&:hover': {
+              backgroundColor: theme.palette.success.dark
+            }
+          }}
+        >
+          Submit
+        </Button>
       </Box>
     </MUIModal>
-  );
-};
+  )
+}
 
-export default NewClaimPopup;
+export default NewClaimPopup
