5b168d64f351f28a7d6ad35be03e98bbbb4a1e57
AhmedAbdelmenam <ahmedabdelmenam534@gmail.com>
2023-04-09 17:57:37 +0200

    add new claim dialoge

--- src/containers/Form/types.ts
+++ src/containers/Form/types.ts
@@ -1,4 +1,6 @@
 interface IHomeProps {
+  open: boolean
+  setOpen: (open: boolean) => void
   toggleSnackbar: (toggle: boolean) => void
   setSnackbarMessage: (message: string) => void
   setLoading: (isLoading: boolean) => void
--- src/containers/Search/index.tsx
+++ src/containers/Search/index.tsx
@@ -5,7 +5,7 @@ import Container from '@mui/material/Container'
 import Box from '@mui/material/Box'
 import TextField from '@mui/material/TextField'
 import Button from '@mui/material/Button'
-import NewClaim from '../../containers/claimNode'
+import NewClaim from '../../components/Add new claim/AddNewClaim'
 import axios from '../../axiosInstance'
 import Modal from '../../components/Modal'
 import cyConfig from './cyConfig'
@@ -193,7 +193,13 @@ const Search = (homeProps: IHomeProps) => {
   return (
     <Container sx={styles.container} maxWidth={false}>
       <Modal open={openModal} setOpen={setOpenModal} selectedClaim={selectedClaim} />
-      <NewClaim open={openNewClaim} setOpen={setOpenNewClaim} />
+      <NewClaim
+        open={openNewClaim}
+        setOpen={setOpenNewClaim}
+        setLoading={setLoading}
+        setSnackbarMessage={setSnackbarMessage}
+        toggleSnackbar={toggleSnackbar}
+      />
       <Box sx={{ position: 'absolute', top: '90px', left: '2%', zIndex: 20 }}>
         <Box
           component='div'
