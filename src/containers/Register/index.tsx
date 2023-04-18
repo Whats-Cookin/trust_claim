73e63dc2bc87150325db7de75194567cd008915a
Sahda Samier <sahdasamier@gmail.com>
2023-04-18 10:00:01 +0200

    use form hook in login file

--- src/containers/Register/index.tsx
+++ src/containers/Register/index.tsx
@@ -1,75 +1,80 @@
 import { useState } from 'react'
 import axios from 'axios'
 import { useNavigate, Link } from 'react-router-dom'
-
-import Box from '@mui/material/Box'
+import { useForm } from 'react-hook-form'
 import Typography from '@mui/material/Typography'
-import TextField from '@mui/material/TextField'
-import Button from '@mui/material/Button'
-
 import IRegisterProps from './types'
 import styles from './styles'
 import { BACKEND_BASE_URL } from '../../utils/settings'
 import polygon1 from '../../assets/circle.png'
 import polygon2 from '../../assets/Polygon 2.png'
 import polygon3 from '../../assets/Polygon 3.png'
+import {TextField, Box, Button, FormControlLabel, Checkbox} from '@mui/material'
+
 
 const Register = ({ toggleSnackbar, setSnackbarMessage, setLoading }: IRegisterProps) => {
-  const [emailRegister, setEmailRegister] = useState('')
-  const [passwordRegister, setPasswordRegister] = useState('')
+  const {
+    register,
+    handleSubmit,
+    reset,
+    formState: {errors},
+  } = useForm()
 
   const navigate = useNavigate()
-
-  const handleRegister = async () => {
+  
+  const onSubmit = handleSubmit( async ({email,password}) => {
     try {
-      if (!emailRegister || !passwordRegister) {
+      if (!email || !password) {
         toggleSnackbar(true)
         setSnackbarMessage('Both email and password are required fields.')
       } else {
-        setLoading(true)
         const signupUrl = `${BACKEND_BASE_URL}/auth/signup`
-        const data = { email: emailRegister, password: passwordRegister }
+        const data = { email , password }
         await axios.post(signupUrl, data)
         setLoading(false)
         navigate('/login')
+        
       }
     } catch (err: any) {
       setLoading(false)
       toggleSnackbar(true)
       setSnackbarMessage(err.response.data.message)
-      console.error('Error: ', err.response.data.message)
+      console.error('err', err.response.data.message)
     }
   }
-
+  )
   return (
     <>
       <img src={polygon1} alt='' style={{ position: 'absolute', top: '3%', left: '-10%' }} />
       <img src={polygon2} alt='' style={{ position: 'absolute', top: '50%', right: '20%' }} />
       <img src={polygon3} alt='' style={{ position: 'absolute', right: '20%', top: '5%', width: '200px' }} />
-      <Box sx={styles.authContainer}>
+      <Box sx={styles.authContainer} >
+        <form onSubmit={onSubmit}>
         <Typography variant='h5' style={{ color: '#80B8BD', textAlign: 'center' }}>
           Register
         </Typography>
-        <TextField
-          value={emailRegister}
+        <TextField 
+         {...register('email' ,{
+          required:true,
+        })}
           fullWidth
           label='Email'
           sx={styles.inputField}
           variant='filled'
-          onChange={(e: any) => setEmailRegister(e.currentTarget.value)}
           type='email'
         />
         <TextField
-          value={passwordRegister}
+        {...register('password', {
+          required:true,
+        })}
           fullWidth
           label='Password'
           sx={styles.inputField}
           variant='filled'
-          onChange={(e: any) => setPasswordRegister(e.currentTarget.value)}
           type='password'
         />
         <Box sx={styles.submitButtonWrap}>
-          <Button onClick={handleRegister} variant='contained' size='medium' sx={styles.submitButton}>
+          <Button  type="submit" variant='contained' size='medium' sx={styles.submitButton}>
             Register
           </Button>
         </Box>
@@ -78,6 +83,7 @@ const Register = ({ toggleSnackbar, setSnackbarMessage, setLoading }: IRegisterP
             Click here to Login
           </Typography>
         </Link>
+        </form>
       </Box>
     </>
   )
