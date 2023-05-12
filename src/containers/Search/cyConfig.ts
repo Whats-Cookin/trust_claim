73106a6da11d2ea441e028559505584cfb0edb9a
Zakia Mangal <zakiaimran513@gmail.com>
2023-05-12 15:48:43 +0430

    changes the color of nodes edges

--- src/containers/Search/cyConfig.ts
+++ src/containers/Search/cyConfig.ts
@@ -11,18 +11,18 @@ const cyConfig = (containerRef: any) => {
           width: 300,
           shape: 'round-rectangle',
           borderRadius: '10%',
-          borderColor: '#006400',
+          borderColor: '#1D686A',
           borderWidth: '10px',
           borderOpacity: 0.5,
-          backgroundColor: '#699e69',
+          backgroundColor: '#1D686A',
           color: '#fff',
-          fontWeight: 'semi-bold',
-          fontSize: 20,
+          fontWeight: 'bold',
+          fontSize: 25,
           textHalign: 'center',
           textValign: 'center',
           textWrap: 'wrap',
           textMaxWidth: 100,
-          textOverflowWrap: 'breakWord',
+          textOverflowWrap: 'break-Word',
           content: 'data(label)'
         }
       },
@@ -32,8 +32,8 @@ const cyConfig = (containerRef: any) => {
           width: 6,
           fontSize: 20,
           targetArrowShape: 'triangle',
-          lineColor: '#006400',
-          targetArrowColor: '#006400',
+          lineColor: '#1D686A',
+          targetArrowColor: '#1D686A',
           curveStyle: 'bezier',
           lineStyle: 'unbundled-bezier',
           controlPointDistances: ' 20px 30px 40px',
@@ -47,13 +47,12 @@ const cyConfig = (containerRef: any) => {
       {
         selector: ':selected',
         style: {
-          backgroundColor: '#0092d9',
-          borderColor: '#0074d9',
+          backgroundColor: '#1D686A',
+          borderColor: '#1D686A',
           borderWidth: 3,
           borderOpacity: 0.5
         }
       },
-
       {
         selector: ':hover',
         style: {
