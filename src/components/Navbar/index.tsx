6f91045721c3e7bd77d1ddb390c5d7bdc7b2f309
AmrNabel <121950000+AmrNabel@users.noreply.github.com>
2023-05-14 16:38:16 +0300

    make navbar responsive with all devices

--- src/components/Navbar/index.tsx
+++ src/components/Navbar/index.tsx
@@ -5,8 +5,9 @@ import Toolbar from '@mui/material/Toolbar'
 import Typography from '@mui/material/Typography'
 import Button from '@mui/material/Button'
 import ProfileDropdown from '../profileDropDown/index'
-import { IconButton, InputBase, Paper, TextField } from '@mui/material'
+import { IconButton, InputBase, Paper, TextField, useMediaQuery } from '@mui/material'
 import SearchIcon from '@mui/icons-material/Search'
+import MenuIcon from '@mui/icons-material/Menu'
 import { useState, useRef, useMemo, useEffect } from 'react'
 import Search from '../../containers/Search'
 
@@ -34,31 +35,20 @@ const Navbar = ({ isAuth }: any) => {
     }
   }
 
+  const isSmallScreen = useMediaQuery('(max-width:1100px)')
+
   return (
     <>
-      <Box
-        sx={{
-          flexGrow: 1,
-          width: '100%',
-          overflow: 'hidden'
-        }}
-      >
-        <AppBar
-          position='fixed'
-          sx={{
-            backgroundColor: '#eeeeee',
-            color: '#280606',
-            top: 0,
-            width: '100%'
-          }}
-        >
+      <Box sx={{ flexGrow: 1 }}>
+        <AppBar position='fixed' sx={{ backgroundColor: '#eeeeee', color: '#280606' }}>
           <Toolbar>
             <Typography
               variant='h5'
               component='div'
               sx={{
                 flexGrow: 1,
-                fontWeight: 'bold'
+                fontWeight: 'bold',
+                textAlign: { xs: 'left', md: 'left' }
               }}
             >
               Trust Claims
@@ -66,18 +56,21 @@ const Navbar = ({ isAuth }: any) => {
             <Box
               sx={{
                 display: 'flex',
-                justifyContent: 'center',
-                columnGap: 3
+                justifyContent: { xs: 'center', md: 'flex-end' },
+                alignItems: 'center',
+                columnGap: 3,
+                flexGrow: { xs: 1, md: 0 }
               }}
             >
-              <>
+              {isSmallScreen ? (
                 <Paper
                   component='div'
                   sx={{
                     p: '2px 4px',
                     display: 'flex',
                     alignItems: 'center',
-                    width: 400
+                    width: '100%',
+                    marginRight: '10px'
                   }}
                 >
                   <InputBase
@@ -95,19 +88,45 @@ const Navbar = ({ isAuth }: any) => {
                     <SearchIcon />
                   </IconButton>
                 </Paper>
+              ) : (
+                <Paper
+                  component='div'
+                  sx={{
+                    p: '2px 4px',
+                    display: 'flex',
+                    alignItems: 'center',
+                    width: '430px',
+                    marginRight: '400px'
+                  }}
+                >
+                  <InputBase
+                    type='search'
+                    value={searchVal}
+                    placeholder='Search a Claim'
+                    onChange={e => setSearchVal(e.target.value)}
+                    onKeyUp={handleSearchKeypress}
+                    sx={{
+                      ml: 1,
+                      flex: 1
+                    }}
+                  />
+                  <IconButton type='button' sx={{ p: '10px' }} aria-label='search' onClick={handleSearch}>
+                    <SearchIcon />
+                  </IconButton>
+                </Paper>
+              )}
 
-                {isAuth && <ProfileDropdown />}
-                {!isAuth && (
-                  <>
-                    <Button color='inherit' onClick={() => navigate('/login')}>
-                      Login
-                    </Button>
-                    <Button color='inherit' onClick={() => navigate('/register')}>
-                      Register
-                    </Button>
-                  </>
-                )}
-              </>
+              {isAuth && <ProfileDropdown />}
+              {!isAuth && (
+                <>
+                  <Button color='inherit' onClick={() => navigate('/login')}>
+                    Login
+                  </Button>
+                  <Button color='inherit' onClick={() => navigate('/register')}>
+                    Register
+                  </Button>
+                </>
+              )}
             </Box>
           </Toolbar>
         </AppBar>
