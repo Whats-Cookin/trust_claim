f80cd06fb44ce7f456da9698d66bb2967e8ada2f
Zakia Mangal <zakiaimran513@gmail.com>
2023-05-17 10:04:09 +0430

    register responsive

--- src/containers/Register/styles.ts
+++ src/containers/Register/styles.ts
@@ -1,19 +1,62 @@
+// const styles = {
+//   authContainer: {
+//     display: 'flex',
+//     flexDirection: 'column',
+//     rowGap: 2,
+//     width: '100%',
+//     padding: '2rem',
+//     maxWidth: '430px',
+//     marginTop: 2,
+//     background: '#FFFFFF',
+//     boxShadow: '0px 1px 20px rgba(0, 0, 0, 0.25)',
+//     zIndex: 20,
+//     borderRadius: '10px'
+//   },
+//   inputField: {
+//     backgroundColor: 'white'
+//   },
+//   submitButtonWrap: {
+//     display: 'flex',
+//     justifyContent: 'flex-end',
+//     width: '100%'
+//   },
+//   submitButton: {
+//     backgroundColor: 'primary.main',
+//     color: 'black',
+//     width: '100%',
+
+//     '&:hover': {
+//       backgroundColor: 'grey.300'
+//     }
+//   }
+// }
+// export default styles
+
 const styles = {
   authContainer: {
     display: 'flex',
     flexDirection: 'column',
     rowGap: 2,
     width: '100%',
-    padding: '2rem',
+    padding: '1rem',
     maxWidth: '430px',
-    marginTop: 2,
+    margin: '0 auto',
     background: '#FFFFFF',
     boxShadow: '0px 1px 20px rgba(0, 0, 0, 0.25)',
     zIndex: 20,
-    borderRadius: '10px'
+    borderRadius: '10px',
+
+    // ...
+    '@media (max-width: 767px)': {
+      padding: '1rem 0.5rem',
+      borderRadius: 0
+    }
   },
+  // ...
+
   inputField: {
-    backgroundColor: 'white'
+    backgroundColor: 'white',
+    width: '100%'
   },
   submitButtonWrap: {
     display: 'flex',
@@ -24,10 +67,10 @@ const styles = {
     backgroundColor: 'primary.main',
     color: 'black',
     width: '100%',
-
     '&:hover': {
       backgroundColor: 'grey.300'
     }
   }
 }
+
 export default styles
