46fbcb70ece1a78dddf66d733cd1e9ff748b3b6d
Golda Velez <golda@3box.io>
2023-05-18 00:23:55 -0700

    we have recompiled and deployed the model to the updated server

--- src/composedb/ceramic_context.tsx
+++ src/composedb/ceramic_context.tsx
@@ -6,7 +6,7 @@ import { RuntimeCompositeDefinition } from '@composedb/types'
 import { CERAMIC_URL } from '../utils/settings.js'
 
 // @ts-ignore
-import { definition } from './__generated__/sample-trustclaims.js'
+import { definition } from './__generated__/trustclaims.js'
 
 /**
  * Configure ceramic Client & create context.
