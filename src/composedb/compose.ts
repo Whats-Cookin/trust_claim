c3630c34dff3a48dc44f70b6dd96e0f083f7259e
Golda Velez <golda@3box.io>
2023-05-19 18:28:37 -0700

    WORKS to write to composedb!!!

--- src/composedb/compose.ts
+++ src/composedb/compose.ts
@@ -30,6 +30,39 @@ type LinkedClaimPayload = {
   stars: number
 }
 
+interface Content {
+  subjectID: string;
+  subjectType?: string;
+  subjectName?: string;
+  claim: string;
+  effectiveDate?: string;
+  statement?: string;
+  object?: string;
+  rating?: {
+    aspect?: string;
+    stars?: number;
+    score?: number;
+  };
+  source?: {
+    sourceID?: string;
+    howKnown?: string;
+    dateObserved?: string;
+    digestMultibase?: string;
+    author?: string;
+    curator?: string; 
+  };
+  amt?: {
+    value?: number,
+    unit?: string,
+    howMeasured?: string
+  };
+  confidence?: number;
+  sharing?: {
+    intendedAudience?: string;
+    respondAt?: string;
+  }
+}
+
 const PublishClaim = async (payload: LinkedClaimPayload): Promise<any> => {
 
   if (!composeClient) {
@@ -39,27 +72,53 @@ const PublishClaim = async (payload: LinkedClaimPayload): Promise<any> => {
 
   const { subject, claim, object, statement, aspect, howKnown, sourceURI, effectiveDate, confidence, stars } = payload
 
-  const variables = {
+  if (! subject || ! claim) {
+     console.log('Subject and claim are required!')
+     return { status: 422 }
+  }
+
+  let edate = '2023-05-19'
+  if (effectiveDate) {
+    edate = effectiveDate.substring(0, 10)
+  }
+
+  const variables: { i: { content: Content } } = {
     "i": {
        "content": {
           "subjectID": subject,
           "claim": claim,
-          "object": object,
-          "rating": { "stars": stars, "score": 0},
-          "source": { "sourceID": sourceURI},
-          "sharing": {
-            "respondAt": '',
-            "intendedAudience": null
-          }, 
-          "statement": statement,
-          "confidence": confidence,
-          "subjectType": null,
-          "effectiveDate": "2023-05-18",
-          "howKnown": howKnown,
-          "aspect": aspect
-        }
+          "effectiveDate": edate
+       }
+    }
+  }
+  
+  if (stars) {
+     variables['i']['content']['rating'] = {
+             "aspect": aspect || 'unknown',
+             "stars": stars || 0, 
+             "score": stars/5.0
      }
   }
+
+  if (sourceURI) {
+    variables['i']['content']['source'] = {
+             "sourceID": sourceURI,
+             "howKnown": howKnown || 'OTHER'
+       }
+  }
+/*          "sharing": {
+            "respondAt": 'https://live.linkedtrust.us/',
+            "intendedAudience": '' 
+          }, 
+        //"subjectType": 'THING',
+*/
+  if (statement) {
+    variables['i']['content']['statement'] = statement
+  }
+  if (confidence) {
+    variables['i']['content']['confidence'] = confidence
+  }
+
 
   const response = await composeClient.executeQuery(CREATE_LINKED_CLAIM_MUTATION, variables)
 
