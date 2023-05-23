b954dfa4aab6efc72d325eb97eb879337ed51486
Golda Velez <golda@3box.io>
2023-05-23 11:26:49 -0700

    copy of search - we want to refactor before really use this

---
+++ src/containers/Feed/graph.utils.ts
@@ -0,0 +1,194 @@
+const isValidUrl = (urlString: string) => {
+  var inputElement = document.createElement('input')
+  inputElement.type = 'url'
+  inputElement.value = urlString
+
+  if (!inputElement.checkValidity()) {
+    return false
+  } else {
+    return true
+  }
+}
+
+const getLabel = (uri: any) => {
+  if (isValidUrl(uri)) {
+    if (uri.hostname === 'trustclaims.whatscookin.us') {
+      return decodeURIComponent(uri.pathname.split('/').pop())
+    }
+    return `Host:\n${uri.origin}\n\n Path:\n${uri.pathname}`
+  } else {
+    return uri
+  }
+}
+
+const parseClaims = (claims: any) => {
+  const elements: any[] = []
+
+  claims.forEach((claim: any) => {
+    // adding subject node
+    if (claim.subject) {
+      let uri: any
+      if (isValidUrl(claim.subject)) uri = new URL(claim.subject)
+      else uri = claim.subject
+
+      const label = getLabel(uri)
+
+      elements.push({
+        data: {
+          id: claim.subject,
+          label: label
+        }
+      })
+    }
+
+    // adding object node
+    if (claim.object) {
+      let uri: any
+      if (isValidUrl(claim.object)) uri = new URL(claim.object)
+      else uri = claim.object
+
+      const label = getLabel(uri)
+
+      elements.push({
+        data: {
+          id: claim.object,
+          label: label
+        }
+      })
+    }
+    // adding edge between subject and object
+    if (claim.subject && claim.object)
+      elements.push({
+        data: {
+          id: claim.id,
+          source: claim.subject,
+          target: claim.object,
+          relation: claim.claim
+        }
+      })
+  })
+  return elements
+}
+
+const parseNodes = (data: any) => {
+  const nodes: any[] = []
+  const edges: any[] = []
+
+  data.forEach((node: any) => {
+    // adding subject node
+    parseNode(nodes, edges, node)
+  })
+  return { nodes, edges }
+}
+
+const getNodeData = (node: any) => {
+  let uri = node.nodeUri
+  // could do this - if we used a trustclaims uri separate the path part
+  // not important - just here for reference from before
+  /*if (isValidUrl(uri)) {
+    let uriObj = new URL(node.nodeUri)
+    if (uriObj.hostname === 'trustclaims.whatscookin.us') {
+      let decodedUri = decodeURIComponent(uri.pathname.split('/').pop())
+      uri = decodedUri.pathname
+    }
+  }*/
+
+  interface NodeData {
+    data: {
+      id: string
+      label: string
+      raw: any
+    }
+    style?: {
+      [key: string]: any
+    }
+  }
+
+  const nodeData: NodeData = {
+    data: {
+      id: node.id.toString(),
+      label: node.name || uri,
+      raw: node
+    }
+  }
+  if (node.entType === 'CLAIM') {
+    nodeData.style = {
+      shape: 'square'
+    }
+  } else {
+    nodeData.style = {
+      shape: 'circle'
+    }
+  }
+
+  if (node.image) {
+    nodeData.style = {
+      'background-image': [node.image.replace(/\?.+$/, '')],
+      'background-fit': 'cover cover',
+      'background-image-opacity': 1.0
+    }
+  } else if (node.thumbnail) {
+    nodeData.style = {
+      'background-image': [node.thumbnail.replace(/\?.+$/, '')],
+      'background-fit': 'cover cover',
+      'background-image-opacity': 0.4
+    }
+  }
+  return nodeData
+}
+
+const parseNode = (nodes: {}[], edges: {}[], node: any) => {
+  // adding subject node
+  if (node.name && node.nodeUri) {
+    const nodeData = getNodeData(node)
+
+    nodes.push(nodeData)
+  }
+
+  // adding edge between subject and object
+  if (node.edgesFrom) {
+    node.edgesFrom.map((e: any) => {
+      if (nodes.indexOf((n: any) => n.id === e.endNode.id.toString()) > -1) return
+
+      const nodeData = getNodeData(e.endNode)
+      nodes.push(nodeData)
+    })
+
+    edges.push(
+      ...node.edgesFrom.map((e: any) => ({
+        data: {
+          id: e.id,
+          source: e.startNodeId,
+          target: e.endNodeId,
+          relation: e.label,
+          raw: e
+        }
+      }))
+    )
+  }
+
+  if (node.edgesTo) {
+    node.edgesTo.map((e: any) => {
+      if (nodes.indexOf((n: any) => n.id === e.startNode.id.toString()) > -1) return
+
+      const nodeData = getNodeData(e.startNode)
+      nodes.push(nodeData)
+    })
+
+    edges.push(
+      ...node.edgesTo.map((e: any) => ({
+        data: {
+          id: e.id,
+          source: e.endNodeId,
+          target: e.startNodeId,
+          relation: e.label,
+          raw: e
+        }
+      }))
+    )
+  }
+
+  return { nodes, edges }
+}
+
+export { parseClaims, parseNodes, parseNode }
---
+++ src/containers/Feed/index.tsx
@@ -0,0 +1,134 @@
+import { useState, useRef, useMemo, useEffect } from 'react'
+import { useLocation, useNavigate } from 'react-router-dom'
+import Container from '@mui/material/Container'
+
+import Box from '@mui/material/Box'
+import Button from '@mui/material/Button'
+import NewClaim from '../../components/NewClaim/AddNewClaim'
+import axios from '../../axiosInstance'
+import Modal from '../../components/Modal'
+import IHomeProps from './types'
+import styles from './styles'
+import SearchIcon from '@mui/icons-material/Search'
+import { parseNode, parseNodes } from './graph.utils'
+import { TextField } from '@mui/material'
+
+const Feed = (homeProps: IHomeProps) => {
+  const navigate = useNavigate()
+  const searchquery = useLocation().search
+  const { setLoading, setSnackbarMessage, toggleSnackbar } = homeProps
+  const ref = useRef<any>(null)
+  const query = new URLSearchParams(searchquery).get('query')
+  const [openModal, setOpenModal] = useState<boolean>(false)
+  const [openNewClaim, setOpenNewClaim] = useState<boolean>(false)
+  const [selectedClaim, setSelectedClaim] = useState<any>(null)
+  const page = useRef(1)
+  const [searchVal, setSearchVal] = useState<string>(query || '')
+  const claimsPageMemo: any[] = []
+
+  const updateClaims = (search: boolean, newClaims: any) => {
+    const parsedClaims = parseNodes(newClaims)
+    // do something else here to update the feed TODO
+  }
+
+  // lets please refactor this out into a function do NOT duplicate between search and feed
+  const fetchClaims = async (query: string, search: boolean, page: number) => {
+    setLoading(true)
+    try {
+      if (search) {
+        const res = await axios.get(`/api/node?page=${page}&limit=5`, {
+          params: { search: query }
+        })
+
+        if (res.data.nodes.length > 0) {
+          updateClaims(search, res.data.nodes)
+        } else {
+          setSnackbarMessage('No results found')
+          toggleSnackbar(true)
+        }
+      } else {
+        // this needs to be encapsulated in a function so we can switch to mock api based on env
+        const res = await axios.get(`/api/node/${query}?page=${page}&limit=5`)
+
+        if (res.data) {
+          let newNodes: any[] = []
+          let newEdges: any[] = []
+          parseNode(newNodes, newEdges, res.data)
+
+          // here is where we can dtry to display the nodes and edges in a feed TODO here
+  
+        } else {
+          setSnackbarMessage('No results found')
+          toggleSnackbar(true)
+        }
+      }
+    } catch (err: any) {
+      toggleSnackbar(true)
+      setSnackbarMessage(err.message)
+    } finally {
+      setLoading(false)
+    }
+  }
+
+  const handleSearch = async () => {
+    window.localStorage.removeItem('claims')
+    if (searchVal.trim() !== '') {
+      navigate({
+        pathname: '/search',
+        search: `?query=${searchVal}`
+      })
+
+      await fetchClaims(encodeURIComponent(searchVal), true, page.current)
+      //page.current = 2
+    }
+  }
+
+  const handleSearchKeypress = async (event: any) => {
+    if (event.key === 'Enter') {
+      handleSearch()
+    }
+  }
+
+  const reset = () => {
+    navigate('/search')
+    setSearchVal('')
+    const ref = useRef<any>(null)
+    const page = useRef(1)
+    page.current = 1
+  }
+
+  const handleNodeClick = async (event: any) => {
+    event.preventDefault()
+    await fetchClaims(event.target.data('id'), false, page.current)
+    //page.current = page.current + 1
+  }
+
+  const handleEdgeClick = (event: any) => {
+    event.preventDefault()
+    const currentClaim = event?.target?.data('raw')?.claim
+
+    if (currentClaim) {
+      setSelectedClaim(currentClaim)
+      setOpenModal(true)
+    }
+  }
+
+
+  return (
+    <Container sx={styles.container} maxWidth={false}>
+      <Modal open={openModal} setOpen={setOpenModal} selectedClaim={selectedClaim} />
+      <NewClaim
+        open={openNewClaim}
+        setOpen={setOpenNewClaim}
+        selectedClaim={selectedClaim}
+        setLoading={setLoading}
+        setSnackbarMessage={setSnackbarMessage}
+        toggleSnackbar={toggleSnackbar}
+      />
+
+      <Box ref={ref} sx={styles.cy} />
+    </Container>
+  )
+}
+
+export default Feed
