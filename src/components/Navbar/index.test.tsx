e55cf62d133d4433b9a0c4c56edb827d71e65c33
Zakia Mangal <zakiaimran513@gmail.com>
2023-06-01 14:53:53 +0430

    navbar and profile testing

---
+++ src/components/Navbar/index.test.tsx
@@ -0,0 +1,57 @@
+import { render, screen, fireEvent } from '@testing-library/react'
+import { MemoryRouter, useNavigate, useLocation } from 'react-router-dom'
+import Navbar from './index'
+import { vi } from 'vitest'
+
+vi.mock('react-router-dom', () => ({
+  useNavigate: vi.fn(),
+  useLocation: vi.fn()
+}))
+
+describe('Navbar component', () => {
+  it('should render correctly when authenticated', () => {
+    const navigate = vi.fn()
+    ;(useNavigate as jest.Mock).mockReturnValue(navigate)
+    ;(useLocation as jest.Mock).mockReturnValue({ search: '' })
+
+    render(<Navbar isAuth={true} />)
+    const trustClaimsText = screen.getByText('Trust Claims')
+    expect(trustClaimsText).toBeInTheDocument()
+
+    const profileDropdown = screen.getByRole('button', { name: '' })
+    expect(profileDropdown).toBeInTheDocument()
+
+    fireEvent.click(profileDropdown)
+    const logoutButton = screen.getByRole('button', { name: /logout/i })
+    expect(logoutButton).toBeInTheDocument()
+    fireEvent.click(logoutButton)
+
+    expect(navigate).toHaveBeenCalledWith('/login')
+  })
+
+  it('should render correctly when not authenticated', () => {
+    const navigate = vi.fn()
+    ;(useNavigate as jest.Mock).mockReturnValue(navigate)
+    ;(useLocation as jest.Mock).mockReturnValue({ search: '' })
+
+    render(<Navbar isAuth={false} />)
+
+    const trustClaimsText = screen.getByText('Trust Claims')
+    expect(trustClaimsText).toBeInTheDocument()
+
+    const searchButton = screen.getByRole('button', { name: /search/i })
+    expect(searchButton).toBeInTheDocument()
+
+    const loginButton = screen.getByRole('button', { name: /login/i })
+    expect(loginButton).toBeInTheDocument()
+
+    fireEvent.click(loginButton)
+    expect(navigate).toHaveBeenCalledWith('/login')
+
+    const registerButton = screen.getByRole('button', { name: /register/i })
+    expect(registerButton).toBeInTheDocument()
+
+    fireEvent.click(registerButton)
+    expect(navigate).toHaveBeenCalledWith('/register')
+  })
+})
---
+++ src/components/profileDropDown/index.test.tsx
@@ -0,0 +1,37 @@
+import { render, fireEvent } from '@testing-library/react'
+import ProfileDropdown from './index'
+import { vi } from 'vitest'
+import { useNavigate } from 'react-router-dom'
+
+vi.mock('react-router-dom', () => ({
+  useNavigate: vi.fn()
+}))
+
+describe('ProfileDropdown component', () => {
+  it('should render correctly', () => {
+    const navigate = vi.fn()
+    ;(useNavigate as jest.Mock).mockReturnValue(navigate)
+
+    const { getByRole, getByText } = render(<ProfileDropdown isAuth={true} />)
+
+    const menuButton = getByRole('button')
+    expect(menuButton).toBeInTheDocument()
+
+    fireEvent.click(menuButton)
+
+    const searchButton = getByText(/Search/i)
+    expect(searchButton).toBeInTheDocument()
+    fireEvent.click(searchButton)
+    expect(navigate).toHaveBeenCalledWith('/search')
+
+    const createClaimButton = getByText(/Create Claim/i)
+    expect(createClaimButton).toBeInTheDocument()
+    fireEvent.click(createClaimButton)
+    expect(navigate).toHaveBeenCalledWith('/')
+
+    const logoutButton = getByText(/Logout/i)
+    expect(logoutButton).toBeInTheDocument()
+    fireEvent.click(logoutButton)
+    expect(navigate).toHaveBeenCalledWith('/login')
+  })
+})
