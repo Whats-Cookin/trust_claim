b34782d697d4c0e2ca4758adc4d219e9e9ed2c64
Zakia Mangal <zakiaimran513@gmail.com>
2023-06-01 19:13:54 +0430

    update clean code

--- src/components/Modal/styles.ts
+++ src/components/Modal/styles.ts
@@ -9,22 +9,19 @@ const styles = {
     width: '100%',
     height: '60%',
     maxHeight: '450px',
-    bgcolor: 'background.paper',
     border: '2px solid #004',
     borderRadius: '4px',
-    boxShadow: 20,
-    p: 4,
-    overflow: 'hidden',
+    boxShadow: '20px',
+    padding: '16px',
     overflowY: 'auto'
   },
   detailField: {
     display: 'flex',
     justifyContent: 'space-between',
     alignItems: 'center',
-    columnGap: '40px',
     borderBottom: '1px solid #000',
     padding: '5px 10px',
-    backgroud: '#eeeeee'
+    background: '#eeeeee'
   },
   fieldContent: {
     overflow: 'hidden',
--- src/components/NewClaim/AddNewClaim.tsx
+++ src/components/NewClaim/AddNewClaim.tsx
@@ -28,3 +28,4 @@ const FormDialog = ({
     </div>
   )
 }
+export default FormDialog
