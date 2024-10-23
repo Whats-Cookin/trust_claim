483533cd4b206af080ce6e4479627ef3c44477c4
Marwan Fouz <marwanfouz76@gmail.com>
2024-10-23 17:45:45 +0300

    feat: evidence to graph button

--- src/components/ClaimReport/index.tsx
+++ src/components/ClaimReport/index.tsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react'
 import axios from 'axios'
-import { useParams } from 'react-router-dom'
+import { useNavigate, useParams } from 'react-router-dom'
 import {
   Container,
   Typography,
@@ -10,11 +10,14 @@ import {
   CircularProgress,
   Box,
   useTheme,
-  useMediaQuery
+  useMediaQuery,
+  Button
 } from '@mui/material'
 import RenderClaimInfo from './RenderClaimInfo'
 import { BACKEND_BASE_URL } from '../../utils/settings'
 import StarIcon from '@mui/icons-material/Star'
+import ShareOutlinedIcon from '@mui/icons-material/ShareOutlined'
+import backSvg from '../../assets/images/back.svg'
 
 interface Claim {
   statement: string | null
@@ -40,6 +43,7 @@ const DonationReport: React.FC = () => {
   const [selectedIndex, setSelectedIndex] = useState<null | number>(null)
   const isMediumScreen = useMediaQuery(theme.breakpoints.down('md'))
   const isLargeScreen = useMediaQuery(theme.breakpoints.up('md'))
+  const navigate = useNavigate()
 
   const url = `${BACKEND_BASE_URL}/api/report/${claimId}`
 
@@ -49,16 +53,25 @@ const DonationReport: React.FC = () => {
       try {
         const response = await axios.get(url)
         setReportData(response.data)
-        console.log('Fetched report data:', response.data)
       } catch (err) {
         setError('Failed to fetch report data')
-        console.error('Error fetching report data:', err)
       } finally {
         setIsLoading(false)
       }
     }
     fetchReportData()
   }, [claimId])
+
+  const handleSchema = async () => {
+    navigate({
+      pathname: '/search',
+      search: `?query=${claimId}`
+    })
+  }
+
+  const handleBack = async () => {
+    navigate('/feed')
+  }
 
   if (isLoading) {
     return (
@@ -210,6 +223,42 @@ const DonationReport: React.FC = () => {
             )}
           </>
         )}
+        <Box sx={{ display: 'flex', justifyContent: 'space-between', mt: '15px' }}>
+          <Button
+            sx={{
+              color: theme.palette.link,
+              textDecoration: 'underline',
+              fontWeight: 500,
+              borderRadius: '100px',
+              fontSize: 'clamp(12px, 2.5vw, 20px)',
+              px: '2rem'
+            }}
+            onClick={handleBack}
+          >
+            <img src={backSvg} alt='arrow' style={{ width: '10px', marginRight: '10px' }} />
+            BACK
+          </Button>
+          <Box>
+            <Button
+              startIcon={<ShareOutlinedIcon />}
+              onClick={() => handleSchema()}
+              variant='text'
+              sx={{
+                p: '4px',
+                fontWeight: 500,
+                borderRadius: '100px',
+                fontSize: 'clamp(5px, 2.5vw, 20px)',
+                px: '2rem',
+                marginRight: '15px',
+                width: { xs: '161px', sm: '226px' },
+                bgcolor: theme.palette.buttons,
+                color: theme.palette.sidecolor
+              }}
+            >
+              Graph View
+            </Button>
+          </Box>
+        </Box>
       </Box>
     </Box>
   )
