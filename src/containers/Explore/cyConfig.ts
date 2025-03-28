0ceeb36104fce514baa1724bd42460a3bb34680a
TutTrue <hamdy.xd55@gmail.com>
2025-03-28 15:23:36 +0200

    Enhance node styles and configurations for various entity types in the Explore container. Added styles for CREDENTIAL, VALIDATION, and AUTHOR nodes, including custom placeholders and labels. Updated cyConfig to support new node types with appropriate styling and templates.

--- src/containers/Explore/cyConfig.ts
+++ src/containers/Explore/cyConfig.ts
@@ -30,6 +30,33 @@ const getNodeStyles = () =>
         height: 80,
         'background-opacity': 0
       }
+    },
+    {
+      selector: 'node[entType="CREDENTIAL"]',
+      style: {
+        shape: 'roundrectangle',
+        height: 120,
+        width: 120,
+        'background-opacity': 0,
+      }
+    },
+    {
+      selector: 'node[entType="VALIDATION"]',
+      style: {
+        shape: 'diamond',
+        height: 100,
+        width: 100,
+        'background-opacity': 0
+      }
+    },
+    {
+      selector: 'node[entType="AUTHOR"]',
+      style: {
+        shape: 'ellipse',
+        height: 120,
+        width: 120,
+        'background-opacity': 0
+      }
     }
   ] as any
 
@@ -81,18 +108,78 @@ const cyConfig = (containerRef: HTMLElement | null, theme: Theme, layoutName: st
   instance.ready(() => {
     instance.nodeHtmlLabel([
       {
-        query: 'node',
+        query: 'node[entType="CLAIM"]',
         halign: 'center',
         valign: 'center',
         valignBox: 'center',
         halignBox: 'center',
-        cssClass: 'custom-node-container',
+        cssClass: 'custom-node-claim',
         tpl: (data: any) => `
-          <div class="custom-node-container">
+          <div class="custom-node-container claim-node">
             ${
               data.image
                 ? `<div class="node-icon" style="background-image: url(${data.image})"></div>`
-                : `<div class="node-placeholder">${getInitials(data.label)}</div>`
+                : `<div class="node-placeholder claim-placeholder">
+                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-circle-check-icon lucide-circle-check"><circle cx="12" cy="12" r="10"/><path d="m9 12 2 2 4-4"/></svg>
+                  </div>`
+            }
+            <div class="node-label">${truncateLabel(data.label, 40)}</div>
+          </div>
+        `
+      },
+      {
+        query: 'node[entType="AUTHOR"]',
+        halign: 'center',
+        valign: 'center',
+        valignBox: 'center',
+        halignBox: 'center',
+        cssClass: 'custom-node-author',
+        tpl: (data: any) => `
+          <div class="custom-node-container author-node">
+            ${
+              data.image
+                ? `<div class="node-icon" style="background-image: url(${data.image})"></div>`
+                : `<div class="node-placeholder author-placeholder">
+                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-user-icon lucide-user"><path d="M19 21v-2a4 4 0 0 0-4-4H9a4 4 0 0 0-4 4v2"/><circle cx="12" cy="7" r="4"/></svg>
+                </div>`
+            }
+            <div class="node-label">${truncateLabel(data.label, 40)}</div>
+          </div>
+        `
+      },
+      {
+        query: 'node[entType="CREDENTIAL"]',
+        halign: 'center',
+        valign: 'center',
+        valignBox: 'center',
+        halignBox: 'center',
+        cssClass: 'custom-node-cred',
+        tpl: (data: any) => `
+          <div class="custom-node-container cred-node">
+            ${
+              data.image
+                ? `<div class="node-icon" style="background-image: url(${data.image})"></div>`
+                : `<div class="node-placeholder cred-placeholder">
+                     <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-medal-icon lucide-medal"><path d="M7.21 15 2.66 7.14a2 2 0 0 1 .13-2.2L4.4 2.8A2 2 0 0 1 6 2h12a2 2 0 0 1 1.6.8l1.6 2.14a2 2 0 0 1 .14 2.2L16.79 15"/><path d="M11 12 5.12 2.2"/><path d="m13 12 5.88-9.8"/><path d="M8 7h8"/><circle cx="12" cy="17" r="5"/><path d="M12 18v-2h-.5"/></svg>
+                   </div>`
+            }
+            <div class="node-label">${truncateLabel(data.label, 40)}</div>
+          </div>
+        `
+      },
+      {
+        query: 'node[entType="VALIDATION"]',
+        halign: 'center',
+        valign: 'center',
+        valignBox: 'center',
+        halignBox: 'center',
+        cssClass: 'custom-node-validation',
+        tpl: (data: any) => `
+          <div class="custom-node-container validation-node">
+            ${
+              data.image
+                ? `<div class="node-icon" style="background-image: url(${data.image})"></div>`
+                : `<div class="node-placeholder validation-placeholder"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-shield-check-icon lucide-shield-check"><path d="M20 13c0 5-3.5 7.5-7.66 8.95a1 1 0 0 1-.67-.01C7.5 20.5 4 18 4 13V6a1 1 0 0 1 1-1c2 0 4.5-1.2 6.24-2.72a1.17 1.17 0 0 1 1.52 0C14.51 3.81 17 5 19 5a1 1 0 0 1 1 1z"/><path d="m9 12 2 2 4-4"/></svg></div>`
             }
             <div class="node-label">${truncateLabel(data.label, 40)}</div>
           </div>
